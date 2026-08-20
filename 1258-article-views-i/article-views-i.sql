e43b0ab453a71790ef00b2778a5f0db4b5980425
Somnath Mondal <somnathmondal2121@gmail.com>
2026-08-20 14:20:30 +0530

    Time: 487 ms (31.45%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,8 +1,6 @@
 # Write your MySQL query statement below
 select v.author_id as id
 from Views as v
-join Views as v1
-on v.article_id   =v1.article_id  
-and v.author_id = v1.viewer_id
+where v.author_id =v.viewer_id 
 group by id
 order by id asc;
