73a075ad78a303688618a39da6eff2110b9ecbc1
Somnath Mondal <somnathmondal2121@gmail.com>
2026-09-06 19:13:29 +0530

    Time: 441 ms (19.79%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select e.employee_id 
+from Employees as e
+left join Employees as m
+on e.manager_id =m.employee_id 
+where e.salary <30000
+    and e.manager_id is not null
+    and m.employee_id is null
+order by e.employee_id ;
